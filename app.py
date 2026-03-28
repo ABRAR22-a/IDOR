c0fc69caee838ac5903bb5834477eee36e29749f
Abara Aldkhail <abraraldkhail@gmail.com>
2026-03-28 14:52:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,4 @@ def get_user():
         return jsonify({"flag": flag})
     return jsonify(users.get(user_id, {"error": "User not found"}))
 
-import os 
-if __name__== "__main__":
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0" , port=port)
+app.run(host="0.0.0.0", port=3000, debug=True)
